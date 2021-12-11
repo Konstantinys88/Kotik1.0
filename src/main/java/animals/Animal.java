62988047ddbca3d2e92221b90235561269f6cd9d
Konstantin0 <maschkovc@yandex.ru>
2021-12-11 15:00:49 +0300

    add satiety

--- src/main/java/animals/Animal.java
+++ src/main/java/animals/Animal.java
@@ -4,5 +4,15 @@ import food.Food;
 
 public abstract class Animal {
 
+    private int Satiety;
+
+    public int getSatiety() {
+        return Satiety;
+    }
+
+    public void setSatiety(int satiety) {
+        Satiety = satiety;
+    }
+
     public abstract int eat(Food food);
 }
