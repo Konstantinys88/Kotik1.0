51898e91d56ff616e252f2c4676661cd5154ed3a
Konstantin0 <maschkovc@yandex.ru>
2021-12-12 13:56:35 +0300

    доработан метод createPond

--- src/main/java/Zoo.java
+++ src/main/java/Zoo.java
@@ -30,17 +30,24 @@ public class Zoo {
         worker.feed(crocodile,meat);
         System.out.println(crocodile.getSatiety());
 
+
+        Swim[]array = Zoo.createPond();
+
+        for (Swim e : array){
+            e.swim();
+        }
+
+
         worker.getVoice(fish);
-
-
     }
 
-    static Animal[] createPond(){
-        Animal[]animals = new Animal[6];
-        for (int i = 0; i < animals.length; i++) {
-
-
-        }
-        return animals;
+     static Swim[] createPond(){
+        Swim[]swim = new Swim[5];
+        swim[0] = new Cow();
+        swim[1] = new Crocodile();
+        swim[2] = new Dog();
+        swim[3] = new Duck();
+        swim[4] = new Fish();
+        return swim;
     }
 }
